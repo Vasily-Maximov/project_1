88825cef79864fdb69b216d35f87532beedc4cc5
Vasily Maximov <softservice-31@mail.ru>
2024-03-17 20:50:56 +0300

    Override method toString in Dog, Cat, Hamster

--- src/main/java/model/Hamster.java
+++ src/main/java/model/Hamster.java
@@ -5,4 +5,12 @@ public class Hamster extends Animal {
     public Hamster(String name, int age) {
         super(name, age);
     }
+
+    @Override
+    public String toString() {
+        return "Hamster{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                '}';
+    }
 }
